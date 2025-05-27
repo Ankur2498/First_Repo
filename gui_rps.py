9f37aaaa3843953ebce3c4a0f11577efa549dbba
Ankur2498 <rajankur321@gmail.com>
2025-05-27 16:32:04 +0530

    Yes and No button made

--- gui_rps.py
+++ gui_rps.py
@@ -5,43 +5,56 @@ window.title("Rock Papers Siccors")
 window.geometry("500x500")
 window.config(bg="black")
 
-
-def game():
-    def IfPlay():
-        
-        label = Label(window,
-                    text="welcome to Rock\n Papers and Sciccors.",
-                    font=("Comic Sans",20,"bold"),
-                    foreground="#00ff00",
+def No():
+    labelNo = Label(window,
+                    text="Thanks for visiting Bye...",
+                    font=("Arial",20),
+                    foreground="cyan",
                     background="black")
-        label.pack()
-        label2 = Label(window,
-                    text="Do you want to play.",
-                    font=("Comic Sans",15),
-                    foreground="white",
-                    background="black",
-                    pady=100)
-        label2.pack()
-        button = Button(window,
-                        text="yes",
-                        font=('Arial',15,'underline'),
-                        foreground="black",
-                        background="white",
-                        activeforeground="black",
-                        activebackground="white",
-                        )
-        button.place(x=100,y=350)
-        button2 = Button(window,
-                        text="no",
-                        font=('Arial',15,'underline'),
-                        foreground="black",
-                        background="white",
-                        activeforeground="black",
-                        activebackground="white",
-                        )
-        button2.place(x=350,y=350)
-    IfPlay()
+    labelNo.pack()
+def Yes():
+    game()
+def game():
+    print("Hi")
+
 game()
+
+def IfPlay():
+        
+    label = Label(window,
+                text="welcome to Rock\n Papers and Sciccors.",
+                font=("Comic Sans",20,"bold"),
+                foreground="#00ff00",
+                background="black")
+    label.pack()
+    label2 = Label(window,
+                text="Do you want to play.",
+                font=("Comic Sans",15),
+                foreground="white",
+                background="black",
+                pady=100)
+    label2.pack()
+    button = Button(window,
+                    command=Yes,
+                    text="yes",
+                    font=('Arial',15,'underline'),
+                    foreground="black",
+                    background="white",
+                    activeforeground="black",
+                    activebackground="white",
+                    )
+    button.place(x=100,y=350)
+    button2 = Button(window,
+                    command=No,
+                    text="no",
+                    font=('Arial',15,'underline'),
+                    foreground="black",
+                    background="white",
+                    activeforeground="black",
+                    activebackground="white",
+                    )
+    button2.place(x=350,y=350)
+IfPlay()
 
 
 window.mainloop()
